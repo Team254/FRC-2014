e480c4108d71692b423aaaf7e94b4230328d9937
Brandon Gonzalez <brandon.gonzalez.451@gmail.com>
2014-01-07 14:43:23 -0800

    Adds customizable serialization to subsystems

--- src/com/team254/lib/Subsystem.java
+++ src/com/team254/lib/Subsystem.java
@@ -1,5 +1,17 @@
 package com.team254.lib;
 
-public class Subsystem {
+import java.util.Hashtable;
+
+public abstract class Subsystem  {
+  protected String name;
+  protected Hashtable data;
   
+  
+  public abstract String serialize();
+    
+  public Subsystem(String name){
+    this.name = name;
+    this.data = new Hashtable();
+    data.put("subsystem", name);
+  }
 }
