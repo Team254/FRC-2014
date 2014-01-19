8ecfec72945659f5e112da861281386cc6b5c341
Stephen Pinkerton <stephen@stephenpinkerton.com>
2014-01-18 23:08:16 -0800

    Add 2013 controller code This has not been tested! The only potential issues may stem from differences between last year's Controller class, and this year's.

---
+++ src/com/team254/lib/ControlSource.java
@@ -0,0 +1,13 @@
+package com.team254.lib;
+
+/**
+ * Interface for subsystem inputs.
+ *
+ * @author richard@team254.com (Richard Lin)
+ */
+public interface ControlSource {
+  public double get();
+  public void updateFilter();
+  public boolean getLowerLimit();
+  public boolean getUpperLimit();
+}
---
+++ src/com/team254/lib/Gains.java
@@ -0,0 +1,11 @@
+package com.team254.lib;
+
+/**
+ * Represents PID gains.
+ *
+ * @author maskoken@gmail.com (Matthew Koken)
+ * @author tom@team254.com (Tom Bottiglieri)
+ */
+public interface Gains {
+  boolean updated();
+}
