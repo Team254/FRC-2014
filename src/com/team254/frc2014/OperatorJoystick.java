37ea7f97422dd290906ce4a362f0d1c0a1966c88
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2014-03-01 14:34:18 -0800

    New control board

--- src/com/team254/frc2014/OperatorJoystick.java
+++ src/com/team254/frc2014/OperatorJoystick.java
@@ -4,75 +4,78 @@ import com.team254.lib.ChezyButton;
 import edu.wpi.first.wpilibj.Joystick;
 
 /**
- * Operator Joystick defines all of the buttons the operator uses on the control panel.
+ * Operator Joystick defines all of the buttons the operator uses on the control
+ * panel.
+ *
  * @author bg
  */
 public class OperatorJoystick extends Joystick {
-  
-  private int catchButtonPort = 1;
-  public ChezyButton catchButton = new ChezyButton(this, catchButtonPort);
-  
-  public void update() {
-    catchButton.update();
-  }
-
-  public boolean getShooterState() {
-    return this.getZ() < 0.0;
-  }
-  
-  public boolean getTrussShotButtonState() {
-    return this.getRawButton(5);
-  }
- 
-  // Intake 
-  public boolean getIntakeButtonState() {
-    return this.getRawButton(7);
-  }
-  public boolean getExhaustButtonState() {
-    return this.getRawButton(8);
-  }
-  public boolean getIntakeUpSwitchState() {
-    return getIntakePositionSwitch() == 1;
-  }
-
-  public boolean getIntakeDownSwitchState() {
-    return getIntakePositionSwitch() == -1;
-  }
-  
-  
-  public boolean getAutoIntakeButtonState() {
-    return this.getRawButton(1);
-  }
-  public boolean getFrontClapperButtonState() {
-    return this.getRawButton(9);
-  }
-  public boolean getRearClapperButtonState() {
-    return this.getRawButton(10);
-  }
-  
-  public int getIntakePositionSwitch() {
-    boolean axis = (this.getRawAxis(4) < 0.1);
-    boolean button = this.getRawButton(Constants.intakeDownSwitchPort.getInt());
-    if (axis) {
-      return 1; // up
-    }
-    if (button) {
-      return -1; // down
-    }
-    return 0; // floating
-  }
 
   public OperatorJoystick(int port) {
     super(port);
   }
 
-  boolean getShotButtonState() {
-    return this.getRawButton(6);
-  }
-  
-  boolean getHoodPositionSwitch() {
-    //if
-    return true;
+  public boolean getAutoCatchButton() {
+    return getRawButton(1);
   }
 
+  public boolean getOpenCatcherButton() {
+    return getRawButton(2);
+  }
+
+  public boolean getShooterOffButton() {
+    return getRawButton(3);
+  }
+
+  public boolean getPreset1Button() {
+    return getRawButton(4);
+  }
+
+  public boolean getPreset2Button() {
+    return getRawButton(5);
+  }
+
+  public boolean getPreset3Button() {
+    return getRawButton(6);
+  }
+
+  public boolean getExhaustButton() {
+    return getRawButton(7);
+  }
+
+  public boolean getIntakeButton() {
+    return getRawButton(8);
+  }
+
+  public boolean getPassRearButton() {
+    return getRawButton(9);
+  }
+
+  public boolean getPassFrontButton() {
+    return getRawButton(10);
+  }
+
+  public boolean getAutoIntakeFrontButton() {
+    return getRawButton(11);
+  }
+
+  public boolean getAutoIntakeRearButton() {
+    return getRawButton(12);
+  }
+
+  public boolean getAutoIntakeOff() {
+    return this.getTwist() < 0.0;
+  }
+
+  public boolean getPreset4Button() {
+    return getZ() < 0;
+  }
+
+  public boolean getPreset5Button() {
+    return getY() > 0;
+  }
+
+  public boolean getPreset6Button() {
+    return getX() < 0;
+  }
 }
