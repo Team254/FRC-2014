0f014ab66576c2af5371127810275412d7b2da1c
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2014-03-08 23:44:18 -0800

    Tweak auto modes for Travus

---
+++ src/com/team254/frc2014/actions/WaitForHotGoalToSwitchAction.java
@@ -0,0 +1,25 @@
+package com.team254.frc2014.actions;
+
+/**
+ * WaitForHotGoalToSwitchAction.java
+ *
+ * @author tombot
+ */
+public class WaitForHotGoalToSwitchAction extends Action {
+  
+  public WaitForHotGoalToSwitchAction(double timeout) {
+    setTimeout(timeout);
+  }
+
+  public boolean execute() {
+    return hotGoalDetector.probablySawGoalChange() || isTimedOut();
+  }
+
+  public void init() {
+    
+  }
+
+  public void done() {
+  }
+
+}
--- src/com/team254/frc2014/auto/TestGooglyEyes.java
+++ src/com/team254/frc2014/auto/TestGooglyEyes.java
@@ -1,11 +1,11 @@
 package com.team254.frc2014.auto;
 
+import com.team254.frc2014.ChezyRobot;
 import com.team254.frc2014.FieldPosition;
 import com.team254.frc2014.ConfigurationAutoMode;
 import com.team254.frc2014.paths.AutoPaths;
 import com.team254.lib.trajectory.Path;
 import edu.wpi.first.wpilibj.Timer;
-
 
 /**
  *
@@ -17,31 +17,57 @@ public class TestGooglyEyes extends ConfigurationAutoMode {
   public TestGooglyEyes() {
     super("TestGooglyEyes");
   }
+
   protected void routine() {
     hotGoalDetector.startSampling();
-    
+
     Timer t = new Timer();
     t.start();
 
     // Turn on wheel
     //shooterController.setVelocityGoal(4000);
-    
+
     shooterController.setVelocityGoal(5300);
     // Grab balls from ground
     clapper.wantFront = false;
     clapper.wantRear = false;
     waitForHotGoalToSwitch(1.5);
-    
+
+
     boolean goLeft = hotGoalDetector.goLeft();
-    System.out.println("go left: "  + goLeft);
+    System.out.println("go left: " + goLeft);
 
     shooterController.setVelocityGoal(0);
-    
 
-    
-    
-    
-    waitTime(5);
+
+
+
+
+    waitTime(1);
+    System.out.println("t: " + autoTimer.get() + " " + ChezyRobot.operatorJoystick.getAutoCatchButton());
+
+    waitTime(1);
+    System.out.println("t: " + autoTimer.get() + " " + ChezyRobot.operatorJoystick.getPreset1Button());
+
+    waitTime(1);
+    System.out.println("t: " + autoTimer.get() + " " + ChezyRobot.operatorJoystick.getPreset1Button());
+
+    waitTime(1);
+     System.out.println("t: " + autoTimer.get() + " " + ChezyRobot.operatorJoystick.getPreset1Button());
+
+    waitTime(1);
+     System.out.println("t: " + autoTimer.get() + " " + ChezyRobot.operatorJoystick.getPreset1Button());
+
+    waitTime(1);
+     System.out.println("t: " + autoTimer.get() + " " + ChezyRobot.operatorJoystick.getPreset1Button());
+
+    waitTime(1);
+     System.out.println("t: " + autoTimer.get() + " " + ChezyRobot.operatorJoystick.getPreset1Button());
+
+    waitTime(1);
+    System.out.println("t: " + autoTimer.get() + " " + ChezyRobot.operatorJoystick.getPreset1Button());
+
+
     clapper.wantFront = false;
     clapper.wantRear = false;
     frontIntake.wantBumperGather = false;
@@ -51,6 +77,4 @@ public class TestGooglyEyes extends ConfigurationAutoMode {
   public FieldPosition getFieldPosition() {
     return null;
   }
-    
- 
 }
