8aff4edda032a4a027f970c765b79afa5dc11394
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2014-03-31 10:15:16 -0700

    Retune 775 shooter

--- src/com/team254/frc2014/ShooterGains.java
+++ src/com/team254/frc2014/ShooterGains.java
@@ -6,12 +6,12 @@ public class ShooterGains {
   static public StateSpaceGains[] getGains() {
     return new StateSpaceGains[] {
         new StateSpaceGains(
-            new double[] {1.000000, 0.000000, 0.605377, 0.988446},  // A
+            new double[] {1.000000, 0.000000, 0.691279, 0.988930},  // A
             new double[] {1.000000, 0.000000},  // B
             new double[] {0.000000, 1.000000},  // C
             new double[] {0.000000},  // D
-            new double[] {0.082593, 0.438446},  // L
-            new double[] {0.968446, 0.318496},  // K
+            new double[] {0.072330, 0.438930},  // L
+            new double[] {0.968930, 0.279589},  // K
             new double[] {12.000000},  // Umax
             new double[] {-0.5000000}),  // Umin
         };
